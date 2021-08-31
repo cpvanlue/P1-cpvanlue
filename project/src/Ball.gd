6076c219ed201a9eb75641f08ffccda526eaa708
Caleb Van Lue <cpvanlue@bsu.edu>
2021-08-31 18:05:59 -0400

    Adds crosshair and laser to assist user with aiming.

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -5,6 +5,7 @@ var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
 
 func _process(delta):
 	angle = clamp(angle, -90, 0)
+	self.rotation_degrees = angle
 	if Input.is_action_pressed("ui_up"):
 		angle -= 5
 	if Input.is_action_pressed("ui_down"):
