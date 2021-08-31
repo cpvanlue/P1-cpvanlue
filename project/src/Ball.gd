b1b9d8edbee947db6133b4db339eb7d9a1d26f4c
Caleb Van Lue <cpvanlue@bsu.edu>
2021-08-31 18:22:53 -0400

    Allows user to only launch ball once before restricting adjustment.

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -2,14 +2,17 @@ extends RigidBody2D
 
 var angle = -45
 var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+var launchValue = 0
 
 func _process(delta):
 	angle = clamp(angle, -90, 0)
 	self.rotation_degrees = angle
 	if Input.is_action_pressed("ui_up"):
-		angle -= 5
+		angle -= 1
 	if Input.is_action_pressed("ui_down"):
-		angle += 5
+		angle += 1
 	if Input.is_action_just_pressed("launch"):
-		impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
-		apply_impulse(Vector2.ZERO, impulse)
+		if launchValue == 0:
+			impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+			apply_impulse(Vector2.ZERO, impulse)
+			launchValue = 1
