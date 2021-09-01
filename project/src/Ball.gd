c36f6d585461043f8608ff0805fea785dc077263
Caleb Van Lue <cpvanlue@bsu.edu>
2021-08-31 21:35:47 -0400

    Includes power bar for user to scope the strength of the projectile's firing.

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,20 +1,27 @@
 extends RigidBody2D
 
 var angle = -45
-var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+var strength = 250
 var launchValue = 0
 
 func _process(delta):
+	$PowerBar.value = strength
+	strength = clamp(strength, 50, 700)
 	angle = clamp(angle, -90, 0)
 	self.rotation_degrees = angle
 	if Input.is_action_pressed("ui_up"):
 		angle -= 1
 	if Input.is_action_pressed("ui_down"):
 		angle += 1
+	if Input.is_action_pressed("ui_left"):
+		strength -= 30
+	if Input.is_action_pressed("ui_right"):
+		strength += 30
 	if Input.is_action_just_pressed("launch"):
-		$laser.visible = false
-		$particle_3.visible = false
+		$Laser.visible = false
+		$Crosshair.visible = false
+		$PowerBar.visible = false
 		if launchValue == 0:
-			impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+			var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * strength
 			apply_impulse(Vector2.ZERO, impulse)
 			launchValue = 1
