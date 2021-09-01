c16e23e9f3e4f0c9cfe49fd17ff6ad12adbda0ef
Caleb Van Lue <cpvanlue@bsu.edu>
2021-08-31 22:27:23 -0400

    Ensures coin is a collidable object rather than a collectible one. Becomes static.

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 var score = 0
 
-func _on_Coin_body_entered(body):
-	$Coin.visible = false
-	score += 10
-	$HUD/ScoreLabel.text = str(score)
-	print("Coin get!")
+
+func _on_Ball_body_entered(body):
+	if body == $Coin:
+		score += 10
+		$HUD/ScoreLabel.text = str(score)
+		print("Coin get!")
