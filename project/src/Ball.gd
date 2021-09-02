8b1d15694b6117f65d0f550ac3b4cdae9cda8325
Caleb Van Lue <cpvanlue@bsu.edu>
2021-09-02 13:04:31 -0400

    Rename strength_factor to strength_increment

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -4,7 +4,7 @@ var angle = -45
 var strength = 250
 var launchValue = 0
 export var degrees_per_second = 50
-export var strength_factor = 30
+export var strength_increment = 30
 
 func _process(delta):
 	$PowerBar.value = strength
@@ -16,9 +16,9 @@ func _process(delta):
 	if Input.is_action_pressed("decrease_angle"):
 		angle += degrees_per_second * delta
 	if Input.is_action_pressed("decrease_strength"):
-		strength -= strength_factor
+		strength -= strength_increment
 	if Input.is_action_pressed("increase_strength"):
-		strength += strength_factor
+		strength += strength_increment
 	if Input.is_action_just_pressed("launch"):
 		mode = RigidBody2D.MODE_RIGID
 		$Laser.visible = false
