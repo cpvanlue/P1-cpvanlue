9fa98cc36a00e3934c658ef3401987d6a9d2ab31
Caleb Van Lue <cpvanlue@bsu.edu>
2021-09-13 16:34:43 -0400

    Give Coin ownership of coin pickup sound rather than Level

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -5,7 +5,7 @@ var score = 0
 
 func _on_Ball_body_entered(body):
 	if body == $Coin:
-		$CoinSound.play()
+		$Coin/CoinSound.play()
 		score += 10
 		$HUD/ScoreLabel.text = str(score)
 		print("Coin get!")
