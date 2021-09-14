5f364cbb80ee1755fdf71d7cf0b29a15c7ed1fc9
Caleb Van Lue <cpvanlue@bsu.edu>
2021-09-14 19:09:03 -0400

    Remove unnecessary variables and method calls

--- project/src/GameOver.gd
+++ project/src/GameOver.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-var score
-
 func _on_Button_pressed():
 	var _ignored = get_tree().change_scene("res://src/MainMenu.tscn")
--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -1,7 +1,4 @@
 extends Node2D
 
-
-
 func _on_Button_pressed():
 	var _ignored = get_tree().change_scene("res://src/Level.tscn")
-	queue_free()
